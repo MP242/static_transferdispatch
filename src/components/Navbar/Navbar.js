5f60d21a759aec8492dd13954186a3d05373ccee
MP242 <marc.pollet@epitech.eu>
2023-02-26 13:08:27 +0400

    css responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Navbar.scss';
 import logo_TD from '../../assets/images/logo_TD.svg';
+import logo_TD_responsive from '../../assets/images/logo_TD_responsive.svg';
 import logo_fb from '../../assets/images/logo_fb.svg';
 import logo_insta from '../../assets/images/logo_insta.svg';
 import logo_twitter from '../../assets/images/logo_twitter.svg';
@@ -10,6 +11,7 @@ class Navbar extends Component {
     return (
       <div className='navbar'>
         <img className='navbar__logo_TD' src={logo_TD} alt="logo transfer dispatch" />
+        <img className='navbar__logo_TD_responsive' src={logo_TD_responsive} alt="logo transfer dispatch" />
         <div className='navbar__logo_reseaux'>
           <img className='navbar__logo_reseaux__img' src={logo_fb} alt=" logo facebook" />
           <img className='navbar__logo_reseaux__img' src={logo_insta} alt=" logo instagram" />
